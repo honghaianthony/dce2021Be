43bd8f0f9d121f41e41194bbb003a1e0ba8c8989
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-09-23 08:03:17 +0700

    add docs swagger

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
-var indexRouter = require('./src/routes/index');
-var usersRouter = require('./src/routes/users');
+const YAML = require('yamljs');
+const swaggerDocs = YAML.load('./docs/swagger.yml');
 
-var app = express();
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
+
+const app = express();
+
+// app.use(cors({
+//     origin: 'https://localhost:3000'
+// }));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api/users', usersRouter);
 
 module.exports = app;
