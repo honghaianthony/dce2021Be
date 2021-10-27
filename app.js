3b8448314122c806ab65fc56fc96296342e108e1
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-10-27 14:12:20 +0700

    connect db, adjust router

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const logger = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 
+const db = require('./src/models/index');
+
 const YAML = require('yamljs');
 const swaggerDocs = YAML.load('./docs/swagger.yml');
 
 const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+// const usersRouter = require('./src/routes/users');
+
+// Configures and sync models to the database
+db.sequelize.sync();
 
 const app = express();
 
-// app.use(cors({
-//     origin: 'https://localhost:3000'
-// }));
+app.use(cors({
+    origin: 'https://localhost:3001'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,8 +28,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api/users', usersRouter);
+// app.use('/api/users', usersRouter);
 
 module.exports = app;
