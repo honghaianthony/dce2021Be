222a802d20bbc72ee447888da6a5e977244593d2
Nguyễn Ngọc Khương Duy <nguyenngockhuongduy172@gmail.com>
2021-12-14 13:47:02 +0700

    socket.io

--- src/realtime/handler.js
+++ src/realtime/handler.js
@@ -6,10 +6,11 @@ module.exports = async (io, socket) => {
     socket.join(room);
   });
   socket.on("send-comment", async (mesage, userId, blogId) => {
+    const blogIdInt = parseInt(blogId);
     const comment = await models.BlogComment.create({
       content: mesage,
       userId: userId,
-      blogId: blogId,
+      blogId: blogIdInt,
     });
     const result = await models.BlogComment.findByPk(comment.id, {
       include: {
@@ -20,10 +21,11 @@ module.exports = async (io, socket) => {
     socket.to(blogId).emit("receive-comment", result);
   });
   socket.on("send-comment-lesson", async (mesage, userId, lessonId) => {
+    const lessonIdInt = parseInt(lessonId);
     const comment = await models.LessonComment.create({
       content: mesage,
       userId: userId,
-      lessonId: lessonId,
+      lessonId: lessonIdInt,
     });
     const result = await models.LessonComment.findByPk(comment.id, {
       include: {
