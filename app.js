8a0791ad02c3fc4d8363ec5503a9060b3a6ccaaf
Duy Nguyễn <nguyenngockhuongduy172@gmail.com>
2022-03-03 15:07:39 +0700

    add auto format code 1

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const swaggerUi = require("swagger-ui-express");
-const cors = require("cors");
-const passport = require("passport");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
+const passport = require('passport');
 
-const initializePassport = require("./src/auth/passport");
+const initializePassport = require('./src/auth/passport');
 
-const db = require("./src/models/index");
+const db = require('./src/models/index');
 
-const YAML = require("yamljs");
+const YAML = require('yamljs');
 // const swaggerDocs = YAML.load("./docs/swagger.yml");
-const swaggerDocs = require("./docs/swagger.json");
+const swaggerDocs = require('./docs/swagger.json');
 
-const indexRouter = require("./src/routes/index");
+const indexRouter = require('./src/routes/index');
 
 // Configures and sync models to the database
 db.sequelize.sync();
@@ -22,29 +22,29 @@ db.sequelize.sync();
 const app = express();
 
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "https://dce2021.vercel.app",
-      "https://dce2021.netlify.app",
-      "https://dce2021.ml"
-    ],
-  })
+    cors({
+        origin: [
+            'http://localhost:3001',
+            'https://dce2021.vercel.app',
+            'https://dce2021.netlify.app',
+            'https://dce2021.ml',
+        ],
+    }),
 );
 
 app.use(
-  logger(":method :url :status :remote-addr - :remote-user [:date[iso]]")
+    logger(':method :url :status :remote-addr - :remote-user [:date[iso]]'),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "src/public")));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 // Auth
 initializePassport(passport);
 app.use(passport.initialize());
 
-app.use("/api", indexRouter);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api', indexRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 module.exports = app;
